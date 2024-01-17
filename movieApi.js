ec53bc3a3c57586ebed2fbb3dfb3715b0b5b5e81
김유진 <78008757+nujinging@users.noreply.github.com>
2024-01-17 13:07:24 +0900

    movieApi 리스트 만들기

--- movieApi.js
+++ movieApi.js
@@ -1,18 +1,11 @@
-$.ajax({
-    type: "get",
-    url: `https://api.themoviedb.org/3/`,
-    dataType: "json",
-}).done((success) => {
-    console.log(success);
-    if (success.code == 1) {
-        alert('1')
+const request = axios.create({
+    baseURL: `https://api.themoviedb.org/3/`,
+    params: {
+        api_key: '',
+        language: 'ko-KR',
     }
 })
-    .fail((err) => {
-        console.log(err)
-    })
-
 
 const movieApi = {
-    nowPlaying : request.get(`movie/now_playing?api_key=`),
-}
+    nowPlaying : () => request.get(`movie/now_playing`),
+};
