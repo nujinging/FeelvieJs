44156334496fe1139476556a8e5dc2cca7c4f742
Ullia <antales21@gmail.com>
2024-01-19 13:16:48 +0900

    movieApi detail 메서드 추가

--- movieApi.js
+++ movieApi.js
@@ -1,11 +1,12 @@
 const request = axios.create({
     baseURL: `https://api.themoviedb.org/3/`,
     params: {
-        api_key: '',
+        api_key: 'c4ae41a953fc79fbf5c13a2d958dfe46',
         language: 'ko-KR',
     }
 })
 
 const movieApi = {
-    nowPlaying : () => request.get(`movie/now_playing`),
+  nowPlaying: () => request.get(`movie/now_playing`),
+  detail: (type, id) => request.get(`/${type}/${id}`),
 };
