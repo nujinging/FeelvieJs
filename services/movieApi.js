a549af6810290e0218ea3f39a3e9e563204d679e
Ullia <antales21@gmail.com>
2024-01-23 15:12:28 +0900

    services/ api detail, social 메서드 추가

--- services/movieApi.js
+++ services/movieApi.js
@@ -1,14 +1,15 @@
 const request = axios.create({
-    baseURL: `https://api.themoviedb.org/3/`,
-    params: {
-        api_key: '',
-        language: 'ko-KR',
-    }
-})
+  baseURL: `https://api.themoviedb.org/3/`,
+  params: {
+    api_key: "",
+    language: "ko-KR",
+  },
+});
 
 const movieApi = {
-    nowPlaying : () => request.get(`movie/now_playing`),
-    popular : () => request.get(`movie/popular`),
-    detail: () => request.get(`movie/now_playing`),
+  nowPlaying: () => request.get(`movie/now_playing`),
+  popular: () => request.get(`movie/popular`),
+  detail: (type, id) => request.get(`${type}/${id}`),
+  social: (type, id) => request.get(`${type}/${id}/external_ids`),
 };
 
