da8c498fe825e582b1f20d8c0e41089b34dc8111
Ullia <antales21@gmail.com>
2024-02-16 16:00:08 +0900

    api 업데이트

--- services/movieApi.js
+++ services/movieApi.js
@@ -7,10 +7,16 @@ const request = axios.create({
 });
 
 const movieApi = {
-  nowPlaying: (type) => request.get(`${type}/now_playing`),
-  popular: (type) => request.get(`${type}/popular`),
+  nowPlaying: type => request.get(`${type}/now_playing`),
+  popular: type => request.get(`${type}/popular`),
   today: (type, time_window) => request.get(`trending/${type}/${time_window}`),
   detail: (type, id) => request.get(`${type}/${id}`),
   social: (type, id) => request.get(`${type}/${id}/external_ids`),
+  credits: (type, id) => request.get(`${type}/${id}/credits`),
+  recommend: (type, id) => request.get(`${type}/${id}/recommendations`),
+  mediaImg: (type, id) =>
+    request.get(`${type}/${id}/images?&language=fr&include_image_language=fr,null,kr`),
+  mediaVid: (type, id) =>
+    request.get(`${type}/${id}/videos?&language=fr&include_image_language=fr,null,kr`),
 };
 
