062f66ffaa43f349a9d0b9fe1b0928f7543e8477
김유진 <78008757+nujinging@users.noreply.github.com>
2024-02-19 13:02:51 +0900

    [Add] genre Api 추가

---
+++ movieApi.js
@@ -0,0 +1,19 @@
+const request = axios.create({
+  baseURL: `https://api.themoviedb.org/3/`,
+  params: {
+    api_key: "",
+    language: "ko-KR",
+  },
+});
+
+const movieApi = {
+  nowPlaying: (type) => request.get(`${type}/now_playing`),
+  popular: (type) => request.get(`${type}/popular`),
+  today: (type, time_window) => request.get(`trending/${type}/${time_window}`),
+  detail: (type, id) => request.get(`${type}/${id}`),
+  social: (type, id) => request.get(`${type}/${id}/external_ids`),
+
+  genreTitle : (type, id) => request.get(`genre/${type}/list`),
+  genreList : (type, genre_number) => request.get(`discover/${type}?with_genres=${genre_number}&sort_by=popularity.desc`)
+};
+
